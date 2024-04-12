9cd97a18a367f191dd334cdbe7f99477497725c0
atulsingh9507 <atulsingh9507@gmail.com>
2024-04-12 12:51:23 +0530

    set limit

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
+from typing import Optional
 
 
 app = FastAPI()
 
-
-@app.get('/')
-
-def index():
-    return {'data': {'data': 'blog list'}}
+@app.get('/blog')
+def index(limit=10, published: bool= True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit}  blogs from the db'}
 
 
 @app.get('/blog/unpublished')
