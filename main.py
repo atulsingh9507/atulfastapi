cac3376f52024b4a3f7c3dd67da7dc821e6f2917
atulsingh9507 <atulsingh9507@gmail.com>
2024-04-12 12:12:18 +0530

    first fastapi add get method

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ app = FastAPI()
 @app.get('/')
 
 def index():
-    return {'data': {'name': 'vikash'}}
+    return {'data': {'data': 'blog list'}}
 
-@app.get('/about')
-def about():
-    return {'data':{'about '}}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blog'}   
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1', '2'}}
+ 
