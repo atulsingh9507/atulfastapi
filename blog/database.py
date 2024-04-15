f3805193c65fc1bcfd15a30c273653157b43145a
atulsingh9507 <atulsingh9507@gmail.com>
2024-04-15 15:49:31 +0530

    add database

--- blog/database.py
+++ blog/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"cheak_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 
 
