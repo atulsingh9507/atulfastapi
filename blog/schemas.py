44099ebb312fd279691116397fd443ce1bada159
atulsingh9507 <atulsingh9507@gmail.com>
2024-04-16 11:02:27 +0530

    show user

--- blog/schemas.py
+++ blog/schemas.py
@@ -9,10 +9,19 @@ class Blog(BaseModel):
 class ShowBlog(BaseModel):
     title: str
     body: str
+
     class Config():
         orm_mode = True
 
 class User(BaseModel):
     name:str
     email:str
-    password:str          
+    password:str         
+
+class ShowUser(BaseModel):
+    name:str
+    email:str
+
+    class Config():
+        orm_mode = True
+               
