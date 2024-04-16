15c5a97ec972e71ddf467e6ca9491a3b5da3364b
atulsingh9507 <atulsingh9507@gmail.com>
2024-04-16 10:09:09 +0530

    Create User

--- blog/main.py
+++ blog/main.py
@@ -60,3 +60,12 @@ def show(id, response: Response, db: Session = Depends(get_db)):
 
 
     return blog
+
+@app.post('/user') 
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name,email=request.email,password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+    
